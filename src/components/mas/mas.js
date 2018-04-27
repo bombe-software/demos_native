1cfa147a46198c891fa5481a968294b590d4be1c
hgwells07 <carlitose07@gmail.com>
2018-04-27 00:03:28 -0500

    corregido de nuevo error de left y body en más

--- src/components/mas/mas.js
+++ src/components/mas/mas.js
@@ -3,7 +3,7 @@ import { View, Alert, ImageBackground, ScrollView, StyleSheet, TouchableHighligh
 
 import { Actions } from 'react-native-router-flux';
 import { Form, Field } from 'react-final-form'
-import { Container, Content, Button, Text, Item, Label, Input, Card, CardItem, List, ListItem } from 'native-base';
+import { Container, Content, Button, Text, Item, Label, Input, Card, CardItem, List, ListItem, Left, Body } from 'native-base';
 
 import { title_light, subtitle_light, image_background, primario, peligro } from './../../../assets/styles';
 
